6a0776b7526ce7905f813d9ace904a4a49c15785
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-20 10:36:39 +0300

    Registered reviews app

--- bookr/settings.py
+++ bookr/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local apps
+    'reviews.apps.ReviewsConfig'
 ]
 
 MIDDLEWARE = [
