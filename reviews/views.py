e32dc7061c7889c462ca3dde388a484215c89b4d
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-21 07:52:30 +0300

    Updated index view

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    name = request.GET.get('name') or 'world'
-    return HttpResponse('Hello, {}!'.format(name))
+    return render(request, '_base.html')
