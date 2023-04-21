963d930492f3f0cfa112f574b54b73249a6bb1f1
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-21 08:31:42 +0300

    Updated index view

--- reviews/views.py
+++ reviews/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def index(request):
     name = 'Josephine'
-    return render(request, '_base.html', {'name': name})
+    return render(request, 'index.html', {'name': name})
