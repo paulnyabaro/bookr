803f72cc0280a5385b9ebbea4fe0fea4e0534d7d
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-24 07:06:27 +0300

    Added string function to all  models

--- reviews/models.py
+++ reviews/models.py
@@ -6,14 +6,23 @@ class Publisher(models.Model):
     website = models.URLField(help_text='The Publisher\'s website')
     email = models.EmailField(help_text='The Publisher\'s email address')
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200, help_text='The title of the book')
     publication_date = models.DateField(verbose_name='Date the book was published')
     isbn = models.CharField(max_length=20, verbose_name='ISBN number of the book')
 
+    def __str__(self):
+        return self.title
+
 
 class Contributor(models.Model):
     first_name = models.CharField(max_length=50, help_text='The contributor\'s first name or names')
     last_name = models.CharField(max_length=50, help_text='The contributor\'s last name or names')
     email = models.EmailField(help_text='The contact email for the contributor')
+
+    def __str__(self):
+        return self.first_name
