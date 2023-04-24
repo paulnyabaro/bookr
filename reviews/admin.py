21b4ab7bcf02f212304e9928b8abdd20c66b15cf
paulnyabaro <paulnyabaro1@gmail.com>
2023-04-24 07:14:22 +0300

    Registered publisher, book and contributor on admin site

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Publisher, Book, Contributor
 
-# Register your models here.
+
+admin.site.register(Publisher)
+admin.site.register(Book)
+admin.site.register(Contributor)
