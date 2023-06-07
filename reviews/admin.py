ad33c6292b97f9070bda057540654d4e556548f1
paulnyabaro <paulnyabaro1@gmail.com>
2023-06-07 06:47:41 +0300

    Registered reviews on admin site

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Publisher, Book, Contributor
+from .models import Publisher, Book, Contributor, Review
 
 
-admin.site.register(Publisher)
 admin.site.register(Book)
 admin.site.register(Contributor)
+admin.site.register(Publisher)
+admin.site.register(Review)
