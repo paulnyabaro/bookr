4c64761a21e35b11a9b0238d08d4003f95c6e7f6
paulnyabaro <paulnyabaro1@gmail.com>
2023-06-09 06:44:12 +0300

    Registered book contributor on admin site

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Publisher, Book, Contributor, Review
+from .models import Publisher, Book, Contributor, Review, BookContributor
 
 
 admin.site.register(Book)
+admin.site.register(BookContributor)
 admin.site.register(Contributor)
 admin.site.register(Publisher)
 admin.site.register(Review)
