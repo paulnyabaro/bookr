6a919e4fc10fba77be4e411efdfdd20e59f07abf
paulnyabaro <paulnyabaro1@gmail.com>
2023-06-09 22:16:06 +0300

    Using custom admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -5,7 +5,7 @@ from reviews.models import Book, BookContributor, Contributor, Publisher, Review
 class BookrAdminSite(AdminSite):
     title_header = 'Bookr Admin'
     site_header = 'Bookr administration'
-    index_title = 'Bookr site admin'
+    index_title = 'Bookr custom site admin'
     
 admin_site = BookrAdminSite(name='bookr')
 
