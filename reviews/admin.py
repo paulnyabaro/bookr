5d6961011ebb3741a44363d9393ee40badbefa63
paulnyabaro <paulnyabaro1@gmail.com>
2023-06-09 07:34:23 +0300

    Using AdminSite

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,9 +1,17 @@
-from django.contrib import admin
+# from django.contrib import admin
+from django.contrib.admin import AdminSite
 from .models import Book, BookContributor, Contributor, Publisher, Review
 
+class BookrAdminSite(AdminSite):
+    title_header = 'Bookr Admin'
+    site_header = 'Bookr administration'
+    index_title = 'Bookr site admin'
+    
+admin_site = BookrAdminSite(name='bookr')
 
-admin.site.register(Book)
-admin.site.register(BookContributor)
-admin.site.register(Contributor)
-admin.site.register(Publisher)
-admin.site.register(Review)
+
+admin_site.register(Book)
+admin_site.register(BookContributor)
+admin_site.register(Contributor)
+admin_site.register(Publisher)
+admin_site.register(Review)
